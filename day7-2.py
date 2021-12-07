59a505808cb19dbe524186276222158c18de0c7f
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2021-12-07 15:07:57 -0500

    Create day7-2.py
    Testing day 7 part 2

---
+++ day7-2.py
@@ -0,0 +1,24 @@
+# Part 1
+with open('day7.py', 'r') as f:
+  ls = list(map(int, f.read().strip().split(',')))
+ 
+numbers = []
+lists = []
+a = 0
+b = 0
+
+for i in range(len(ls)):
+  for line in ls:
+    if ls[i] > line:
+      b = (ls[i] - line)
+      numbers.append(sum([j for j in range(a, b+1))
+    else:
+      b = (line - ls[i])
+      numbers.append(sum([j for j in range(a, b+1))
+  lists.append(numbers)
+  numbers = []
+    
+for item in range(len(lists)):
+  lists[item] = sum(lists[item])
+lists.sort()
+print(lists[:1])  
