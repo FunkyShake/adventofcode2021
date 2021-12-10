9dfd7ad735b4b4d70b4ece56a15b23daf77a06be
FunkyShake <75706045+FunkyShake@users.noreply.github.com>
2021-12-10 14:07:33 -0500

    Update day8.py
    added part 2

--- day8.py
+++ day8.py
@@ -1,3 +1,6 @@
+from itertools import permutations # for part 2
+
+# part 1
 with open('day8.txt', 'r') as f:
   ls = f.read().strip().split('\n')
   
@@ -13,3 +16,31 @@ for item in splitls:
     pass
   
 print(count)
+
+# part 2
+totals = 0
+# set example in integer order 0-9
+example = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+ex_set = set(example)
+
+for line in ls:
+    left, right = line.split(' | ')
+    
+    for perm in permutations("abcdefg"):
+      
+      #making a dictionary of every letter combination
+      dictionaries = {k: v for k, v in zip(perm, 'abcdefg')}
+      
+      # making a set of possible dictionary combinations
+      l = {''.join(sorted(map(dictionaries.get, item))) for item in left.split()}
+      
+      if l == ex_set:
+        
+        # make a list of dictionary items for right side
+        right = [''.join(sorted(map(dictionaries.get, item]))) for item in right.split()]
+        # make a string based off the index in example
+        right = ''.join(str(example.index(x) for x in right)
+        # convert to integer and add to totals
+        totals += int(right)
+                        
+print(totals)
